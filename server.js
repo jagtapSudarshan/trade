db35731403f946ca3bb87bebcf973d1e290e0733
sudarshan jagtap <>
2019-04-26 01:14:15 +0530

    refactoring

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("test success");
+console.log("Build started ");
 const express = require('express')
 // const bodyParser= require('body-parser')
 const app = express()
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 const db_url = "mongodb://root:uniken123$@ds117336.mlab.com:17336/nodetestmongodb";
 
 var db
-var serverPort = 8080;
+var serverPort = 4000;
 var fs = require('fs');
 
 
@@ -43,10 +43,11 @@ MongoClient.connect(db_url, (err, database) => {
 
 
 app.get('/', function(req, res) {
-   res.sendFile('/Users/sudarshanjagtap/Documents/Sud/Document/BackendAndDemos/NodeWithMongo/web' + '/index.html')
+  //  res.sendFile('/Users/sudarshanjagtap/Documents/Sud/Document/BackendAndDemos/NodeWithMongo/web' + '/index.html')
 })
+
 app.get('/optiondata', function(req, res) {
-  // res.sendFile('/Users/sudarshanjagtap/Documents/Sud/Document/BackendAndDemos/NodeWithMongo/web' + '/2.txt')
+
   var ip = (req.headers['x-forwarded-for'] ||
   req.connection.remoteAddress ||
   req.socket.remoteAddress ||
@@ -84,22 +85,9 @@ app.get('/optiondata', function(req, res) {
     var jsonData = [firstOBJ,secondOBJ,thirdOBJ,fourthOBJ,fifthOBJ,emptyOBJ];
     res.send(jsonData);
     
-  
-    // var spot = tableData[0][0][1].split(":")[1].split(" ")[2];
-    // res.send({'SPOTValue': parseInt(spot),"optionchain":tableData}) 
-    /*
-       tableData === 
-        [ 
-          [ 
-            { State: 'Minnesota', 'Capitol City': 'Saint Paul', 'Pop.': '3' },
-            { State: 'New York', 'Capitol City': 'Albany', 'Pop.': 'Eight Million' } 
-          ] 
-        ]
-    */
   });
 
  console.log(ip);
-  // res.send({'response':'APPA ROCKS'}) 
 })
 
 function getTime (addHour, addMin){
@@ -160,145 +148,5 @@ function getTime (addHour, addMin){
   var am = (AM)?'AM':'PM';
   return new Array(newHour, newMin, am);
 };
-
-// Allow cross origin resource sharing (CORS) within our application
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/Users/sudarshan/Documents/BackendAndDemos/NodeWithMongo/'); // Absolute path. Folder must exist, will not be created for you..
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) 
-  }
-})
-
-var upload = multer({ storage: storage });
-
-app.post('/upLoadProfileImage', upload.single('image'), function (req, res, next) {
-
-    var imagePath = '/Users/sudarshan/Documents/BackendAndDemos/NodeWithMongo/'+req.file.filename
-
-      db.collection("quotes").findAndModify(
-        { Name: "sud" },     // query
-        [],               // represents a sort order if multiple matches
-        { $set: {image_url:imagePath} },   // update statement
-        { new: true },    // options - new to return the modified document
-        function(err,doc) {
-
-        }
-       );   
-    res.send(imagePath)   
-});
-
-app.post('/addImage', (req, res) => {
-    let base64String = req.body.image;
-    let base64Image = base64String.split(';base64,').pop();
-    let imgName = 'image_'+Date.now()+'.png';
-    fs.writeFile('./public/images/'+imgName, base64Image, {encoding: 'base64'}, function(err) {
-        console.log('File created');
-        db.collection("quotes").findAndModify(
-          { Name: "sud" },     // query
-          [],               // represents a sort order if multiple matches
-          { $set: {image_url:imgName} },   // update statement
-          { new: true },    // options - new to return the modified document
-          function(err,doc) {
-            var fullImgPath = 'http://'+req.host+':'+serverPort+'/images/'+imgName;
-            res.send({'imageURL':fullImgPath})  
-          }
-         );  
-    });
-});
-
-app.post('/createProfile', (req, res) => {
-  let username = req.body.name;
-  let userpass = req.body.password;
-  let city = req.body.city;
-  let zipcode = req.body.zipcode;
-  let dob = req.body.dob;
-  let base64ImageString = req.body.image;
-  let base64Image = base64ImageString.split(';base64,').pop();
-  let imgName = 'image_'+Date.now()+'.png';
-  fs.writeFile('./public/images/'+imgName, base64Image, {encoding: 'base64'}, function(err) {
-      console.log('File created');
-
-          var fullImgPath = 'http://'+req.host+':'+serverPort+'/images/'+imgName;
-          // res.send({'imageURL':fullImgPath})  
-          db.collection('userProfile').findOne({name:username},function(err,result){
-            if (err){ 
-              console.log('error')
-              res.send({'status':'error'})
-            }else if(!result){
-              db.collection('userProfile').insert({name:username,userpass:userpass,city:city,zipcode:zipcode,dob:dob,image:fullImgPath} ,(err, result) => {
-                if (err) return console.log(err)
-                console.log('saved to database')
-                res.send('success')
-              })
-            }else{
-              res.send({'status':'fails'})
-              console.log('fail to saved')
-            }
-          }) 
-  });
-});
-
-app.post('/v2/getProfile', (req, res) => {
-  let username = req.body.name;
-  db.collection('userProfile').find({"name": req.body.name}).toArray(function(err,result){
-
-    res.send(result)
-  });
-});
-
-
-
-app.post('/addUser', (req, res) => {
-  var userName = req.body.Name;
-  var userPass = req.body.Password;
-  db.collection('quotes').findOne({Name:userName,Password:userPass},function(err,result){
-    if (err){ 
-      console.log('error')
-      res.send({'status':'error'})
-    }else if(!result){
-      db.collection('quotes').insert(req.body, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.send('success')
-      })
-    }else{
-      res.send({'status':'fails'})
-      console.log('fail to saved')
-    }
-  })
-});
-
-app.get('/getAllUser', (req, res) => {
-  var cursor = db.collection('quotes').find()
-  db.collection('quotes').find().toArray(function(err, results) {
-    console.log(results)
-    res.send(results)
-  }) 
-})
-
-app.post('/verifyUser', (req, res) => {
-  var userName = req.body.Name;
-  var userPass = req.body.Password;
-  db.collection('quotes').findOne({Name:userName,Password:userPass},function(err,result){
-    if (err){       
-      res.send({'status':'error'})
-      console.log('error')
-    }else if(!result){
-      res.send({'status':'fails'})
-      console.log('fail to verify, invalid creds')
-    }else{
-      res.send({'status':'success'})
-      console.log('success to verify')
-    }
-  })
-});
 
 
